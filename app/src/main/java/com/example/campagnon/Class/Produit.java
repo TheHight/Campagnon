d909fc48d727a978f3d4af50965470a1f6d10810
maxime <maximemaitre@hotmail.fr>
2020-06-09 00:45:25 +0200

    Passage en POO

--- app/src/main/java/com/example/campagnon/Class/Produit.java
+++ app/src/main/java/com/example/campagnon/Class/Produit.java
@@ -7,4 +7,47 @@ public class Produit {
     private String nom_produit;
     private String qté_produit;
     private String prix_kg;
+
+    public Produit() {
+    }
+
+    public String getType_produit() {
+        return type_produit;
+    }
+
+    public void setType_produit(String type_produit) {
+        this.type_produit = type_produit;
+    }
+
+    public String getImage() {
+        return image;
+    }
+
+    public void setImage(String image) {
+        this.image = image;
+    }
+
+    public String getNom_produit() {
+        return nom_produit;
+    }
+
+    public void setNom_produit(String nom_produit) {
+        this.nom_produit = nom_produit;
+    }
+
+    public String getQté_produit() {
+        return qté_produit;
+    }
+
+    public void setQté_produit(String qté_produit) {
+        this.qté_produit = qté_produit;
+    }
+
+    public String getPrix_kg() {
+        return prix_kg;
+    }
+
+    public void setPrix_kg(String prix_kg) {
+        this.prix_kg = prix_kg;
+    }
 }
